e850780b83589865c78dab7facd6071f84d314bf
Ratiporn <ratiporn.pr@gmail.com>
2023-11-10 12:06:14 +0700

    fix: type declare

--- src/interface/ErrorType.ts
+++
@@ -1,5 +0,0 @@
-export function createUnknownActionError(message?: string): Error {
-  const error = new Error(message || "Action unknown");
-  error.name = "UnknownActionError";
-  return error;
-}
